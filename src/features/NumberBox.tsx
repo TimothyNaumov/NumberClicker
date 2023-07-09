3d02b93c325f04d2a01dd92543f34a210289a3c5
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2023-07-08 22:36:24 -0500

    feat: number text changes with win and lose as well as color

---
+++ src/features/NumberBox.tsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/material";
+
+export const NumberBox = ({ gameState }: any) => {
+  if (gameState.endGameState === "WON") {
+    return (
+      <Typography variant="h1" color="success">
+        You Won!
+      </Typography>
+    );
+  } else if (gameState.endGameState === "LOSE") {
+    return (
+      <Typography variant="h1" color="error">
+        {gameState.randomNumber}
+      </Typography>
+    );
+  }
+  return <Typography variant="h1">{gameState.randomNumber}</Typography>;
+};
+
+export default NumberBox;
---
+++ src/features/ResetButton.tsx
@@ -0,0 +1,19 @@
+import { Button, IconButton } from "@mui/material";
+import ReplayIcon from "@mui/icons-material/Replay";
+
+export const ResetButton = ({ gameState }: any) => {
+  if (gameState.endGameState === "WON" || gameState.endGameState === "LOSE") {
+    return (
+      <Button color="primary" variant="contained" onClick={gameState.resetGame}>
+        Play Again
+      </Button>
+    );
+  }
+  return (
+    <IconButton onClick={gameState.resetGame}>
+      <ReplayIcon />
+    </IconButton>
+  );
+};
+
+export default ResetButton;
