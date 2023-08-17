41b15869ee521aad56a6d1b17e3ebbdd2ee150a0
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2023-08-16 21:46:59 -0500

    hooked up chart to real time database

--- src/views/StatsView.tsx
+++ src/views/StatsView.tsx
@@ -1,47 +1,101 @@
 import { Box, Theme, Typography } from '@mui/material';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Text } from 'recharts';
 import { useTheme } from '@emotion/react';
+import React, { useEffect } from 'react';
+import { database } from '../Firebase';
+import { off, onValue, ref } from 'firebase/database';
 
-const sampleData = [
-  { score: 1, frequency: 2 },
-  { score: 2, frequency: 1 },
-  { score: 3, frequency: 2 },
-  { score: 4, frequency: 2 },
-  { score: 5, frequency: 7 },
-  { score: 6, frequency: 4 },
-  { score: 7, frequency: 7 },
-  { score: 8, frequency: 2 },
-  { score: 9, frequency: 6 },
-  { score: 10, frequency: 0 },
-];
+function formatData(data: any) {
+  const formattedData: any[] = [];
+  for(let i = 1; i <= 10; i++){
+    const freq = {
+      score: i,
+      frequency: data[i]
+    }
+    formattedData.push(freq);
+  }
+  return formattedData;
+}
 
-const StatsView = ({data}: any) => {
+const StatsView = ({data, average, gamesPlayed}: any) => {
   const theme = useTheme() as Theme;
+  
   return (
     <Box
       display="flex"
       flexDirection="column"
       alignItems="center"
       justifyContent="space-between"
+      
     >
-      <Box>
-        <Typography variant='h3'>8/15/2024 Snapshot</Typography>
+      <Box p={2} m={2}>
+        <Typography variant='h3'>Live Global Data</Typography>
       </Box>
-      <Box>
-        <BarChart width={1200} height={600} data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="score" label={{ value: 'Score', angle: 0, position: 'bottom', dy: -15 }} />
-          <YAxis label={{ value: 'Frequency', angle: -90, position: 'insideLeft' }}/>
-          <Tooltip itemStyle={{ color: 'black' }}/>
-          <Bar dataKey="frequency" fill={theme.palette.primary.main} />
-        </BarChart>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="flex-start"
+        justifyContent="space-between"
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="flex-start"
+          justifyContent="flex-start"
+          width={300}
+        >
+          <Typography variant='h4'>Average: {average.toFixed(2)}</Typography>
+          <Typography variant='h4'>Games Played: {gamesPlayed}</Typography>
+        </Box>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Box>
+            <BarChart width={1200} height={600} data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="score" label={{ value: 'Score', angle: 0, position: 'bottom', dy: -15 }} />
+              <YAxis label={{ value: 'Frequency', angle: -90, position: 'insideLeft' }}/>
+              <Tooltip itemStyle={{ color: 'black' }}/>
+              <Bar dataKey="frequency" fill={theme.palette.primary.main} />
+              <Text x={0} y={20} textAnchor="start" fill="black" fontSize="24px" fontWeight="bold">Average</Text>
+              <Text x={0} y={60} textAnchor="start" fill="black" fontSize="24px" fontWeight="bold">Games Played</Text>
+            </BarChart>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
 };
 
 const Resolver = () => {
-  return <StatsView data={sampleData}/>;
+  const [stats, setStats] = React.useState(null) as any;
+
+  useEffect(() => {
+    const db = database;
+    const statsRef = ref(db, 'stats');
+
+    const handleValueChange = (snapshot: any) => {
+      console.log(snapshot.val());
+      setStats(snapshot.val());
+    }
+    onValue(statsRef, handleValueChange);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      off(statsRef, 'value', handleValueChange);
+    };
+  }, []);
+
+  if(stats == null){
+    return <p>loading...</p>
+  }
+
+  console.log(formatData(stats.frequencyDistribution));
+
+  return <StatsView data={formatData(stats.frequencyDistribution)} average={stats.averageScore} gamesPlayed={stats.gamesPlayed}/>;
 }
 
 export default Resolver;
